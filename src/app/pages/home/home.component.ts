25fa35af8fad59746c34f35c34af337710be532f
Mariano <macacozzz@hotmail.com>
2024-05-23 18:43:57 -0300

    initial commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './home.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent { }
