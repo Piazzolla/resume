26bbba450b4595922155013af9ea3e64e1bda9de
Mariano <macacozzz@hotmail.com>
2024-07-22 17:48:27 -0300

    sibar stickyness and hiding in small screens - bigger about text - added job experience in data

---
+++ src/app/resume/components/summary/info-card/info-card.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'summary-info-card',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './info-card.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InfoCardComponent {
+  @Input() title: string = 'Title';
+  @Input() subtitle: string = 'Subtitle';
+  @Input() description: string = 'Description';
+  @Input() logo: string = 'logo';
+
+ }
--- src/app/resume/components/summary/summary.component.ts
+++ src/app/resume/components/summary/summary.component.ts
@@ -1,20 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TextService } from '../../services/text.service';
+import { InfoCardComponent } from "./info-card/info-card.component";
 
 @Component({
-  selector: 'app-summary',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './summary.component.html',
-  styles: `
+    selector: 'app-summary',
+    standalone: true,
+    templateUrl: './summary.component.html',
+    styles: `
     :host {
       display: block;
     }
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        CommonModule,
+        InfoCardComponent
+    ]
 })
 export default class SummaryComponent {
 
